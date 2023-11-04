d01ad4fcecb358c53a0b6f77240628cf8d2c3eb0
akii12 <114606455+akii12@users.noreply.github.com>
2023-11-04 12:28:47 +0530

    Add files via upload

---
+++ src/todoList.js
@@ -0,0 +1,77 @@
+import React, { useState } from 'react'
+import Category from './components/category/Category';
+import DeleteBtn from './components/deleteBtn/DeleteBtn';
+import Form from './components/form/Form';
+import Task from './components/task/Task';
+
+function TodoList() {
+
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 'Do coding challenges',
+                taskText: 'Do coding challenges',
+                isCompleted: false
+            },
+            {
+                id: 'learn DSA',
+                taskText: 'learn DSA',
+                isCompleted: false
+            },
+            {
+                id: 'learn React',
+                taskText: 'learn React',
+                isCompleted: false
+            },
+        ])
+
+    const [activeCategory, setActiveCategory] = useState('all')
+    let tasksList = createTasksList(tasks);
+
+    function handleCategory(event) {
+        const id = event.target.id;
+        setActiveCategory(() => id)
+    }
+
+    const activeTasksList = tasks.filter(task => task.isCompleted === false)
+    const completedTasksList = tasks.filter(task => task.isCompleted === true)
+
+    if (activeCategory === 'all') {
+        tasksList = createTasksList(tasks)
+    } else if (activeCategory === 'active') {
+        tasksList = createTasksList(activeTasksList)
+    } else if (activeCategory === 'completed') {
+        tasksList = createTasksList(completedTasksList)
+    }
+
+    function createTasksList(arr) {
+        return arr.map(task => (
+            <Task
+                key={task.id}
+                taskText={task.taskText}
+                tasks={tasks}
+                setTasks={setTasks}
+                isCompleted={task.isCompleted}
+            />
+        ))
+    }
+
+    return (
+        <div className="my-8 max-w-2xl w-[90%] mx-auto relative h-screen">
+            <main>
+                <h1 className='font-raleway text-center text-4xl font-bold tracking-tighter mb-14'>#todo</h1>
+                <Category
+                    activeCategory={activeCategory}
+                    handleCategory={handleCategory}
+                />
+                <Form setTasks={setTasks} />
+                {tasksList}
+                {activeCategory === 'completed' && <DeleteBtn setTasks={setTasks} />}
+            </main>
+
+
+        </div>
+    );
+}
+
+export default TodoList;
